b8506a70a233df7bc417a1ea814d1405426b1321
MasterHexada <sajgutierrez12@gmail.com>
2026-07-25 13:08:42 -0500

    PR4: parametros de ejecucion agregados, reestructuracion y encapsulamiento de screen

--- src/services/ShootService.py
+++ src/services/ShootService.py
@@ -2,9 +2,10 @@ from src.model.Bullet import Bullet
 from src.model.Player import Player
 
 class ShootService:
-    def __init__(self) -> None:
+    def __init__(self, bullet_speed) -> None:
         self.__cooldown_counter = 0
         self.bulletsList = list()
+        self.bullet_speed = bullet_speed
 
     def increment_counter (self) -> None:
         self.__cooldown_counter += 1
@@ -13,12 +14,14 @@ class ShootService:
         # Disparo de balas cada 20 frames
         if self.__cooldown_counter >= 20:
             self.__cooldown_counter = 0
-            self.bulletsList.append(Bullet(player.x, player.y, data=Bullet.actual_ammo))
+            self.bulletsList.append(Bullet(player.x, player.y,
+                                            data=Bullet.actual_ammo, bullet_speed=self.bullet_speed))
             
     def draw_bullets (self, screen) -> None: 
         # Actualizar y dibujar cada proyectil
         for bullet in self.bulletsList[:]:
-            bullet.move_up(speed=20)
+            bullet: Bullet
+            bullet.move_up()
             bullet.bullet_react.center = (bullet.x, bullet.y)
             screen.blit(bullet.image, bullet.bullet_react)
     
